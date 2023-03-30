6c1685cba9f4eef8eb2737b42272939863ffb2d8
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2023-03-29 22:13:26 -0400

    chg description header to match roxygen2

--- scripts/seascapes_extract.R
+++ scripts/seascapes_extract.R
@@ -1,30 +1,45 @@
-extract_seascapes <- function(filename, df) {
 ##%######################################################%##
 #                                                          #
 ####         Extract Seascapes from seascapes           ####
 #                                                          #
 ##%######################################################%##
-# ---- DESCRIPTION: ------
-# Description
-#
-# ---- INPUTS: -----------
-# Input =  
-#
-# ---- OUTPUTS: ----------
-# Output = 
-#
-# ---- NOTES: ------------
-#
-# ---- REFERENCES(s): ----
-#
-# ---- AUTHOR(s): --------
-# Anna Finch (2022-08-31)
-    sea <- raster(filename) %>%
-        tabularaster::as_tibble(xy = T) %>%
+extract_seascapes <- function(filename, df) {
+    #' Extract Seascapes from seascapes
+    #'
+    #' FUNCTION_DESCRIPTION
+    #'
+    #' @param filename DESCRIPTION.
+    #' @param df DESCRIPTION.
+    #'
+    #' @return RETURN_DESCRIPTION
+    #' 
+    #' @author Anna Finch (2022-08-31)
+    #' @examples
+    #' # ADD_EXAMPLES_HERE
+    
+    
+    librarian::shelf(
+        librarian, tibble, tidyr, readr, purrr, dplyr, stringr,
+        forcats, lubridate, glue, fs, magrittr, here,
+        # broom # optional
+        
+        # additional
+        tabularaster, raster, sp, geosphere
+    )
+    
+    library("conflicted")
+    
+    conflict_prefer("filter", "dplyr")
+    conflict_prefer("select", "dplyr")
+    
+    sea <- 
+        raster(filename) %>%
+        tabularaster::as_tibble(xy = TRUE) %>%
         drop_na(cellvalue)
     
-    coord_df <- df %>%
-        distinct(station, .keep_all = T) %>%
+    coord_df <- 
+        df %>%
+        distinct(station, .keep_all = TRUE) %>%
         dplyr::select(lon, lat)
     
     #extract coordinates for every station
@@ -32,7 +47,7 @@ extract_seascapes <- function(filename, df) {
         sp::SpatialPointsDataFrame(coords = cbind(coord_df$lon,coord_df$lat),
                                    proj4string = CRS(SRS_string = "EPSG:4326"))
     
-    #make a list of all the seascapes classes for each station in the cruise
+    # make a list of all the seascapes classes for each station in the cruise
     seascape_list <- list()
     for (i in 1:length(coord)) {
         temp_dist <- geosphere::distGeo(
@@ -40,14 +55,19 @@ extract_seascapes <- function(filename, df) {
             p2 = coord[i,]
         )
         
-        #temporary dataframe with distances, seascapes, coordinates
-        temp_df <- sea %>% mutate(dist = as.numeric(temp_dist)) %>%
-            #filter for distances less than 12 km (3-pixel radius, pixels are 4km x 4km)
+        # temporary dataframe with distances, seascapes, coordinates
+        temp_df <- 
+            sea %>% 
+            mutate(dist = as.numeric(temp_dist)) %>%
+            
+            # filter for distances less than 12 km (3-pixel radius, pixels are 4km x 4km)
             filter(temp_dist < 12000) %>%
             group_by(cellvalue) %>%
-            #calculate average inverse distance for each seascape class
+            
+            # calculate average inverse distance for each seascape class
             summarise(sum1 = sum(1/dist)/n()) %>%
-            #calculate probabilities of coordinate being the present seascape classes
+            
+            # calculate probabilities of coordinate being the present seascape classes
             mutate(prob = sum1 / sum(sum1))
         
         #select row with the greatest probability
