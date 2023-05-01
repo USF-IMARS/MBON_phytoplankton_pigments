3f3f138170f2003ccb6207e6a1dc6fe840b31c48
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2023-05-01 01:59:20 -0400

    undo

--- scripts/krig_fun.R
+++ scripts/krig_fun.R
@@ -264,7 +264,6 @@ map_data <- function(
                    color = "yellow") +
         
         labs(color = NULL,
-             fill = "Concentration",
              title = .title) +
         
         scale_fill_gradientn(breaks = breaks, 
